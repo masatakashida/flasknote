4b24c896548f95fb42839dc32c20a1cb724d3f4e
masataka <ar.tec.masa@gmail.com>
2018-03-24 01:10:13 +0900

    haml

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+from hamlish_jinja import HamlishExtension
+from werkzeug import ImmutableDict
+
+class FlaskWithHamlish(Flask):
+	jinja_options = ImmutableDict(
+		extensions=[HamlishExtension]
+	)
+app = FlaskWithHamlish(__name__)
 
 @app.route('/')
 def hello_world():
-	return render_template('index.html')
+	return render_template('index.haml', username="John")
