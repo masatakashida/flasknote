15f863cd6c80ce3d409c9d17c0b8958420d6ad75
masataka <ar.tec.masa@gmail.com>
2018-03-24 01:25:47 +0900

    sql

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from hamlish_jinja import HamlishExtension
 from werkzeug import ImmutableDict
+import os
+import sqlite3
 
 class FlaskWithHamlish(Flask):
 	jinja_options = ImmutableDict(
@@ -10,4 +12,22 @@ app = FlaskWithHamlish(__name__)
 
 @app.route('/')
 def hello_world():
-	return render_template('index.haml', username="John")
+	entries = get_db().execute('select title, body from entries').fetchall()
+	return render_template('index.haml', entries=entries)
+
+# Database
+def connect_db():
+	db_path = os.path.join(app.root_path, 'flasknote.db')
+	rv = sqlite3.connect(db_path)
+	rv.row_factory = sqlite3.Row
+	return rv
+
+def get_db():
+	if not hasattr(g, 'sqlite_db'):
+		g.sqlite_db = connect_db()
+	return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+	if hasattr(g, 'sqlite_db'):
+		g.sqlite_db.close()
